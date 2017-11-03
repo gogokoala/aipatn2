3cca3cce160a3286b26cb70434c712f4a888ef9d
steps.ding <372258@qq.com>
2017-11-03 19:50:29 +0800

    format sf1 complex

--- client/src/app/sf1/complex-search/complex-search.component.ts
+++ client/src/app/sf1/complex-search/complex-search.component.ts
@@ -72,50 +72,47 @@ export class ComplexSearchComponent implements OnInit {
     },
   ];
 
-  exp=new ComplexSearchExp();
+  exp = new ComplexSearchExp();
 
   date_group: any[] = [
     { id: 1, name: '', title: '申请日'},
     { id: 2, name: '', title: '公开（公告）日'},
     { id: 3, name: '', title: '授权日'},
   ];
-  
-  
-  
 
   constructor(private router: Router) {
-    let k: any[] = [
-      { id: 1, name: ["名称","摘要","权利要求书","说明书"], title: '所有字段' },
-      { id: 2, name: ["名称","摘要"], title: '专利名称/摘要' },
-      { id: 3, name: ["名称","摘要","权利要求书"], title: '专利名称/摘要/权利要求'},
-      { id: 4, name: ["名称"], title: '专利名称'},
-      { id: 5, name: ["摘要"], title: '摘要'},
-      { id: 6, name: ["权利要求书"], title: '权利要求'},
-      { id: 8, name: ["说明书"], title: '说明书'},
+    const k: any[] = [
+      { id: 1, name: ['名称', '摘要', '权利要求书', '说明书'], title: '所有字段' },
+      { id: 2, name: ['名称', '摘要'], title: '专利名称/摘要' },
+      { id: 3, name: ['名称', '摘要', '权利要求书'], title: '专利名称/摘要/权利要求'},
+      { id: 4, name: ['名称'], title: '专利名称'},
+      { id: 5, name: ['摘要'], title: '摘要'},
+      { id: 6, name: ['权利要求书'], title: '权利要求'},
+      { id: 8, name: ['说明书'], title: '说明书'},
     ];
     this.exp.initKeyGroup(k);
 
-    let c: any[] = [
-      { id: 1, name: ["申请号"], title: '申请号' },
-      { id: 2, name: ["公开（公告）号"], title: '公开（公告）号' },
-      { id: 3, name: ["优先权"], title: '优先权号' },
+    const c: any[] = [
+      { id: 1, name: ['申请号'], title: '申请号' },
+      { id: 2, name: ['公开（公告）号'], title: '公开（公告）号' },
+      { id: 3, name: ['优先权'], title: '优先权号' },
     ];
     this.exp.initCodeGroup(c);
 
-    let t: any[] = [
-      { id: 1, name: ["分类号"], title: '国际分类号（IPC）' },
+    const t: any[] = [
+      { id: 1, name: ['分类号'], title: '国际分类号（IPC）' },
       { id: 2, name: [], title: '外观分类(Locarno)' },
     ];
     this.exp.initTypeGroup(t);
 
-    let n: any[] = [
-      { id: 1, name: ["申请（专利权）人"], title: '申请（专利权）人' },
+    const n: any[] = [
+      { id: 1, name: ['申请（专利权）人'], title: '申请（专利权）人' },
       { id: 2, name: [], title: '当前专利权人' },
       { id: 3, name: [], title: '股票代码' },
-      { id: 4, name: ["发明（设计）人"], title: '发明人' },
-      { id: 5, name: ["代理人"], title: '代理人' },
-      { id: 6, name: ["专利代理机构"], title: '代理机构' },
-      { id: 8, name: ["地址"], title: '申请人地址' },
+      { id: 4, name: ['发明（设计）人'], title: '发明人' },
+      { id: 5, name: ['代理人'], title: '代理人' },
+      { id: 6, name: ['专利代理机构'], title: '代理机构' },
+      { id: 8, name: ['地址'], title: '申请人地址' },
     ];
     this.exp.initNameGroup(n);
   }
@@ -130,7 +127,7 @@ export class ComplexSearchComponent implements OnInit {
   }
 
   // valToString(){
-  //   let v="";
+  //   let v='';
   //   v = JSON.stringify(this.key_group);
   //   return v;
   // }
--- client/src/app/sf1/complex-search/complex-search.service.ts
+++ client/src/app/sf1/complex-search/complex-search.service.ts
@@ -1,125 +1,139 @@
 
 export class ComplexSearchConditionItem {
-  op: string;
-  value: string;
+  op: string
+  value: string
 
   constructor() {
-    this.op = "AND";
-    this.value = "";
+    this.op = 'AND'
+    this.value = ''
   }
 }
 
 export class ComplexSearchCondition {
-  id: number;
-  name: Array<string>;
-  title: string;
-  items = Array<ComplexSearchConditionItem>();
+  id: number
+  name: Array<string>
+  title: string
+  items = Array<ComplexSearchConditionItem>()
 
   constructor(id: number, name: Array<string>, title: string) {
-    this.id = id;
-    this.name = name;
-    this.title = title;
+    this.id = id
+    this.name = name
+    this.title = title
 
-    this.newItem();
+    this.newItem()
   }
 
   newItem() {
-    let v = new ComplexSearchConditionItem();
-    this.items.push(v);
+    const v = new ComplexSearchConditionItem()
+    this.items.push(v)
   }
 
   removeItemAt(index) {
-    this.items.splice(index, 1);
+    this.items.splice(index, 1)
   }
 
   getValue() {
-    let v = "";
+    let v = ''
     for (let i = 0; i < this.items.length; i++) {
-      let it = this.items[i];
-      if (it.value.trim() != "") {
-        if (v != "") v += (" " + it.op + " ")
+      const it = this.items[i]
+      if (it.value.trim() !== '') {
+        if (v !== '') {
+          v += (' ' + it.op + ' ')
+        }
         v += it.value
       }
     }
 
-    if (v == "") return "";
-
-    let r = "";
-
-    for (let i = 0; i < this.name.length; i++) {
-      if (i > 0) r += " and ";
-      r += (this.name[i] + "=(" + v + ")");
+    if (v === '') {
+      return ''
     }
 
-    return r;
+    let r = ''
+
+    for (let i = 0; i < this.name.length; i++) {
+      if (i > 0) {
+        r += ' and '
+      }
+      r += (this.name[i] + '=(' + v + ')')
+    }
+
+    return r
   }
 
 }
 
 export class ComplexSearchExp {
-  key_group = Array<ComplexSearchCondition>();
-  code_group = Array<ComplexSearchCondition>();
-  type_group = Array<ComplexSearchCondition>();
-  name_group = Array<ComplexSearchCondition>();
+  key_group = Array<ComplexSearchCondition>()
+  code_group = Array<ComplexSearchCondition>()
+  type_group = Array<ComplexSearchCondition>()
+  name_group = Array<ComplexSearchCondition>()
 
   constructor() {
   }
 
-  private initGroup(data:any[],group:Array<ComplexSearchCondition>){
+  private initGroup(data: any[], group: Array<ComplexSearchCondition>){
     for (let i = 0; i < data.length; i++) {
-      let cond = new ComplexSearchCondition(data[i].id, data[i].name, data[i].title);
-      group.push(cond);
+      const cond = new ComplexSearchCondition(data[i].id, data[i].name, data[i].title)
+      group.push(cond)
     }
   }
-  
+
   initKeyGroup(data: any[]) {
-    this.initGroup(data,this.key_group);
+    this.initGroup(data, this.key_group)
   }
 
   initCodeGroup(data: any[]) {
-    this.initGroup(data,this.code_group);
+    this.initGroup(data, this.code_group)
   }
 
   initTypeGroup(data: any[]) {
-    this.initGroup(data,this.type_group);
+    this.initGroup(data, this.type_group)
   }
 
   initNameGroup(data: any[]) {
-    this.initGroup(data,this.name_group);
+    this.initGroup(data, this.name_group)
   }
 
   private getValueByGroup(group: Array<ComplexSearchCondition>) {
-    let v = "";
+    let v = ''
 
-    for (var i = 0; i < group.length; i++) {
-      let r = group[i].getValue();
-      if (r != "") {
-        if (v != "") v += " and ";
-        v += r;
+    for (let i = 0; i < group.length; i++) {
+      const r = group[i].getValue()
+      if (r !== '') {
+        if (v !== '') {
+          v += ' and '
+        }
+        v += r
       }
     }
 
-    return v;
+    return v
   }
 
   getValue() {
-    let k = this.getValueByGroup(this.key_group);
-    let c = this.getValueByGroup(this.code_group);
-    let t = this.getValueByGroup(this.type_group);
-    let n = this.getValueByGroup(this.name_group);
+    const k = this.getValueByGroup(this.key_group)
+    const c = this.getValueByGroup(this.code_group)
+    const t = this.getValueByGroup(this.type_group)
+    const n = this.getValueByGroup(this.name_group)
 
-    let v = k;
+    let v = k
 
-    if (c != "" && v != "") v += " and ";
-    v += c;
+    if (c !== '' && v !== '') {
+      v += ' and '
+    }
+    v += c
 
-    if (t != "" && v != "") v += " and ";
-    v += t;
+    if (t !== '' && v !== '') {
+      v += ' and '
+    }
+    v += t
 
-    if (n != "" && v != "") v += " and ";
-    v += n;
+    if (n !== '' && v !== '') {
+      v += ' and '
+    }
+    v += n
 
-    return v;
+    return v
   }
 
 
