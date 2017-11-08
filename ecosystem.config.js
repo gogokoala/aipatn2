f98358f3bd973e57b6dcdf409220338417ba21b1
gogokoala <372258@qq.com>
2017-11-08 10:34:32 +0800

    debug ecosystem 1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -47,7 +47,7 @@ module.exports = {
       user : 'node',
       host : 'www.aipatn.com',
       ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
+      repo : 'git@github.com:gogokoala/aipatn2.git',
       path : '/mnt/disk1/www/development',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
       env  : {
